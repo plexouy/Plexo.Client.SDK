14f300cf2c5b2f0d9f96aee019bcd2ca6392a65c
Maximo Piva <maximo.piva@gmail.com>
2018-03-12 12:04:09 -0300

    Added missing issuer client creation.

--- PaymentGatewayClientFactory.cs
+++ PaymentGatewayClientFactory.cs
@@ -5,6 +5,7 @@ namespace Plexo.Client.SDK
     public class PaymentGatewayClientFactory
     {
         private static readonly Dictionary<string,PaymentGatewayClient> Instances=new Dictionary<string, PaymentGatewayClient>();
+        private static readonly Dictionary<string, PaymentGatewayClient> InstancesIssuer = new Dictionary<string, PaymentGatewayClient>();
 
         public static PaymentGatewayClient GetClient(string clientname)
         {
@@ -15,6 +16,14 @@ namespace Plexo.Client.SDK
                 return Instances[clientname];
             }
         }
-
+        public static PaymentGatewayClient GetIssuer(string issuername)
+        {
+            lock (InstancesIssuer)
+            {
+                if (!InstancesIssuer.ContainsKey(issuername))
+                    InstancesIssuer[issuername] = PaymentGatewayClient.CreateClientForIssuer(Properties.Settings.Default.PaymentServerUrl, issuername);
+                return InstancesIssuer[issuername];
+            }
+        }
     }
 }
